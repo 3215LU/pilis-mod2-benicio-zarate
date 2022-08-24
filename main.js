1eeb2cb5ea0c50f3621ee09f5cb2822af666a37c
cristian zarate <cristianzarate@outlook.com>
2022-08-24 14:19:25 -0300

    api add

--- main.js
+++ main.js
@@ -30,10 +30,8 @@ function onClick (event) {
           /* redirectUrl(); */
       })
       .catch((err) => console.log(err));
-  }
+}
 
-
-  
 function cleanForm() {
     let formulario = document.getElementById('formulario');    
     formulario.reset();    
@@ -46,3 +44,30 @@ let boton = document.getElementById("enviar");
 boton.addEventListener("click", onClick);
 
 
+let contenedor = document.getElementById("clima");
+let datos = null;
+
+fetch("https://api.openweathermap.org/data/2.5/weather?lat=-24.184014695495744&lon=-65.33153392590026&appid=160031d61625f9bc412611ff76438779&units=metric&lang=es")
+  .then(response => response.json())
+  .then(response => {
+    datos = response;
+    mostrarDatos(datos)
+  })
+  .catch(error => console.log(error))
+
+function mostrarDatos(datos) {
+    let temperatura = document.createElement("p");
+    let humedad = document.createElement("p");
+    let termica = document.createElement("p");
+    let descripcion = document.createElement("p");
+    
+    temperatura.innerHTML = "Temperatura: " + datos.main.temp + " c";
+    humedad.innerHTML = "Humedad: " + datos.main.humidity + "%";
+    termica.innerHTML = "Sensación térmica: " + datos.main.feels_like + " c";
+    descripcion.innerHTML = "Cielo: " + datos.weather[0].description;
+
+    contenedor.appendChild(temperatura);
+    contenedor.appendChild(humedad);
+    contenedor.appendChild(termica);
+    contenedor.appendChild(descripcion);
+}
